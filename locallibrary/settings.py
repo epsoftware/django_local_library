3d2cb906a7536012e3ddead6984abcc24958186c
Edgar Pineda <edgarpineda.dev@gmail.com>
2020-11-19 19:57:37 -0500

    settings.py updated'

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / 'staticfiles' # os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
